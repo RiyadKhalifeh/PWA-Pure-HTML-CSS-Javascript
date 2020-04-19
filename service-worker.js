f708a411b9e6760915466e009b868be99ff97e52
Riyad Khalifeh <RiyadKhalifehIT@gmail.com>
2020-04-19 19:16:02 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -44,16 +44,3 @@ self.addEventListener("fetch", function(event) {
     );
 });
 
-self.addEventListener("activate", function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    if (CACHE_NAME !== cacheName && cacheName.startsWith("gih-cache")) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
