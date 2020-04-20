f8316def273665eebbbdf228166ac1af3c56ed77
Riyad Khalifeh <RiyadKhalifehIT@gmail.com>
2020-04-20 19:39:04 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
-/*
+function _isMobile(){
+    // if we want a more complete list use this: http://detectmobilebrowsers.com/
+    // str.test() is more efficent than str.match()
+    // remember str.test is case sensitive
+    var isMobile = (/iphone|ipod|android|ie|blackberry|fennec/).test
+         (navigator.userAgent.toLowerCase());
+    return isMobile;
+}
+
+if(!_isMobile()){
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
@@ -17,4 +26,4 @@ window.addEventListener('beforeinstallprompt', (e) => {
       });
 
 });
-*/
+}
