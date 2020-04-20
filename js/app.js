b0b7cccc4f873e830626c1f6dece350f8b2ee630
Riyad Khalifeh <RiyadKhalifehIT@gmail.com>
2020-04-20 19:29:43 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,12 +4,6 @@
   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
-
- 
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
